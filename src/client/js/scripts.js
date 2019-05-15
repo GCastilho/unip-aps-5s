194a5e3052e096acda34239cac74d84f93ff923b
João Victor <joaojvdsvictor@gmail.com>
2019-05-15 10:27:13 -0300

    Refatoração do script JS
    --implementado função para carregar mensagens ao iniciar a pagina
    --alterado path absoluto para path relativo no arquivo html
    --implementado armazenamento local

--- src/client/js/scripts.js
+++ src/client/js/scripts.js
@@ -1,48 +1,84 @@
+//configurações da scrollbar
 $('.left-section, .message').mCustomScrollbar({
     scrollInertia: 85
 });
 
-let updateScroll = () => {
-    var element = document.getElementsByClassName("message");
-    element.scrollTop = element.scrollHeight;
-};
-
-var historic = () => {
-    $('.chat').append("<li class=\"msg-left\">" +
-        "                        <div class=\"msg-left-sub\">" +
-        "                            <img src=\"./../img/man03.png\">" +
-        "                            <div class=\"msg-desc\">" +
-        "                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod" +
-        "                                tempor incididunt ut labore et dolore magna aliqua." +
-        "                            </div>" +
-        "                            <small>05:01 am</small>" +
-        "                        </div>" +
-        "                    </li>")
-    updateScroll();
+//função para mudar o posicionamento do scrollbar
+let scrollUpdate = () => {
     $('.message').mCustomScrollbar("scrollTo", "bottom",{
-        scrollInertia: 0,
-
+        scrollInertia: 0
     });
 };
 
-$(() => {
-    $('.btn-send').click(() => {
-        if ($('#inputMessage').val() !== "") {
-            let dt = new Date();
+//função para carregar as mensagens ao iniciar a pagina
+window.onload = () => {
+    if (localStorage.getItem('key') === null) {
+        $.getJSON("../js/data.json", (data) => {
+            chatGenerator(data);
+        });
+    } else {
+        chatGenerator(JSON.parse(localStorage.getItem('key')));
+    }
+};
+
+//função para gerar as mensagens e adicionar ao HTML
+let chatGenerator = (data) => {
+    console.log(data);
+    for(var i = 0; i < data.length; i++) {
+        if (data[i].id === "1") {
             $('.chat').append("<li class=\"msg-right\">" +
                 "                        <div class=\"msg-left-sub\">" +
                 "                            <img src=\"./../img/man04.png\">" +
-                "                            <div class=\"msg-desc\">" + $('#inputMessage').val() +
+                "                            <div class=\"msg-desc\">" + data[i].message +
                 "                            </div>" +
-                "                            <small>"+dt.getHours()+":" +String(dt.getMinutes()).padStart(2, '0')+"</small>" +
+                "                            <small>"+data[i].time+"</small>" +
                 "                        </div>" +
                 "                    </li>");
+            scrollUpdate();
+        } else {
+            $('.chat').append("<li class=\"msg-left\">" +
+                "                        <div class=\"msg-left-sub\">" +
+                "                            <img src=\"./../img/man03.png\">" +
+                "                            <div class=\"msg-desc\">" + data[i].message +
+                "                            </div>" +
+                "                            <small>"+data[i].time+"</small>" +
+                "                        </div>" +
+                "                    </li>");
+            scrollUpdate();
         }
-        historic();
-        updateScroll();
-        $('.message').mCustomScrollbar("scrollTo", "bottom",{
-            scrollInertia: 0,
+    }
+};
 
-        });
+$(() => {
+    //função de envio
+    $('.btn-send').click(() => {
+        if ($('#inputMessage').val() !== "") {
+            if (localStorage.getItem('key') === null) {
+                $.getJSON("../js/data.json", (data) => {
+                    let dt = new Date();
+                    data.push({
+                        id: "1",
+                        message: $('#inputMessage').val(),
+                        time: dt.getHours() + ":" + String(dt.getMinutes()).padStart(2, '0')
+                    });
+                    localStorage.setItem('key', JSON.stringify(data));
+                });
+            } else {
+                let data = JSON.parse(localStorage.getItem('key'));
+                let dt = new Date();
+                data.push({
+                    id: "1",
+                    message: $('#inputMessage').val(),
+                    time: dt.getHours() + ":" + String(dt.getMinutes()).padStart(2, '0')
+                });
+                localStorage.clear();
+                localStorage.setItem('key', JSON.stringify(data));
+            }
+        scrollUpdate();
+    }});
+
+    //função para teste, usada para limpar o armazenamento local
+    $('.teste').click(() => {
+        localStorage.clear();
     });
 });
