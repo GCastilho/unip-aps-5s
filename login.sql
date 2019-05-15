4694628cbb43afb96c9b4b4202ad75672eb4f8a2
gabriel <gabrielgustavorodrigues@gmail.com>
2019-05-15 01:14:45 -0300

    added a return to addCookie metode
    --addCookie now has a string as return , now it return the sessionID 
    --fixed the login function, before it returned the inverse of the result
    --added time stamp to cookie table and now the method addCookie also sends the time stamp for the  database
    --minor changes
    --TO DO: change login sql statement for a prepared statement, change the method addCookie name to makeCookie (can i?) and the return variable(password) to something else

--- login.sql
+++ login.sql
@@ -6,6 +6,7 @@ CREATE TABLE login_data (
 CREATE TABLE cookie (
     ID int NOT NULL AUTO_INCREMENT  PRIMARY KEY,
     user_name varchar(255) NOT NULL,
-    sessionID varchar(130)
+    sessionID varchar(130),
+    timeStamp varchar(30)
 );
 insert into login_data(ID,name,password_enc) values(0,'gabriel','FA585D89C851DD338A70DCF535AA2A92FEE7836DD6AFF1226583E88E0996293F16BC009C652826E0FC5C706695A03CDDCE372F139EFF4D13959DA6F1F5D3EABE');
--- src/server/login/Login.java
+++ src/server/login/Login.java
@@ -9,7 +9,7 @@ public class Login {
 
 	public static void main(String[] args) {
 		if (validCredentiais("gabriel","FA585D89C851DD338A70DCF535AA2A92FEE7836DD6AFF1226583E88E0996293F16BC009C652826E0FC5C706695A03CDDCE372F139EFF4D13959DA6F1F5D3EABE")){
-			System.out.print("logado");
+			System.out.println("logado");
 		}else{
 			System.out.print("erro");
 		}
@@ -18,45 +18,45 @@ public class Login {
 
 	public static boolean validCredentiais(String user, String password){
 		password = getSHA512(password);
-		String query = "select id from login_data where name = '"+user+"' and password_enc ='"+password+"'";
+		String query = "select count(id) from login_data where name = '"+user+"' and password_enc ='"+password+"'";
 		Connection con = DatabaseConnection.getConexaoMySQL();
 		try{
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
 			int count = 0;
 			while (rs.next()){
-			count = rs.getInt("id");
+			count = rs.getInt("count(id)");
 			}
-			if (count != 1)
-			{
+			if (count == 1){
 				return true;
-			} else 
-			{
+			}else {
 				return false;
 			}
 		}catch(SQLException e){ e.printStackTrace();}
 		return false;
 	}
 	
-	public static void addCookie(String nome,String password){
+	public static String addCookie(String user,String password){
 		Connection con = DatabaseConnection.getConexaoMySQL();
 
 		Date date= new Date();
 		long time = date.getTime();
-		Timestamp ts = new Timestamp(time);			
+		Timestamp ts = new Timestamp(time);
 
-		password =  getSHA512(password+nome+ts);
+		password =  getSHA512(password+user+ts);
 
-		String query = " insert into cookie (user_name,sessionID)"
-		+ " values (?,?)";
+		String query = " insert into cookie (user_name,sessionID,timeStamp)"
+		+ " values (?,?,?)";
 		try{
 			PreparedStatement preparedStmt = con.prepareStatement(query);
-			preparedStmt.setString (1,nome );
-			preparedStmt.setString (2,password);     
+			preparedStmt.setString (1,user );
+			preparedStmt.setString (2,password);
+			preparedStmt.setString (3,""+ts+"");
 
 			preparedStmt.execute();}catch(SQLException e){ e.printStackTrace();
 		}
 		System.out.println("sessao adicionada com sucesso ao banco de dados");
+		return password;
 	}
 
 	public static String getSHA512(String input){
