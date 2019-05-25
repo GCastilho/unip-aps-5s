0f1212b087559d3bd8d2fb3bf210900b2db189ab
Gabriel Castilho <castilho.gabriel14@gmail.com>
2019-05-25 16:56:10 -0300

    Minor fixes
    -- Convertidos a classe Http para package-private, assim como a maioria de seus métodos
    -- Adicionada função getCookie na Http
    -- Retirado algumas redundâncias e códigos velhos da HttpAppHandler
    -- Reindentado App e Roo Handler para tab

--- src/http/HttpRootHandler.java
+++ src/http/HttpRootHandler.java
@@ -8,45 +8,45 @@ import database.DatabaseConnection;
 import java.io.IOException;
 
 public class HttpRootHandler implements HttpHandler {
-    public void handle(HttpExchange httpExchange) throws IOException {
-        if (httpExchange.getRequestMethod().equals("GET")) {
-            if (httpExchange.getRequestURI().getPath().equals("/")) {//
-                Http.sendHtml(httpExchange, "/index.html");
-            } else {
-                Http.sendRaw(httpExchange, httpExchange.getRequestURI().getPath());
-            }
-        } else if (httpExchange.getRequestMethod().equals("POST")) {
-            String username = null;
-            String password = null;
-            {
-                String query = Http.getPOST(httpExchange.getRequestBody());
-                String[] array = query.split("&");
-                for (String str : array) {
-                    String[] pair = str.split("=");
-                    if (pair[0].equals("username")) {
-                        username = pair[1];
-                    } else if (pair[0].equals("password")) {
-                        password = pair[1];
-                    }
-                }
-            }
-            try {
-                if (DatabaseConnection.validCredentials(username, password)) {
-                    String sessionID = DatabaseConnection.makeCookie(username, password);
-                    Headers headers = httpExchange.getResponseHeaders();
-                    headers.set("Set-Cookie", String.format("%s=%s; path=/app", "sessionID", sessionID));
-                    headers.set("Location", "/app");
+	public void handle(HttpExchange httpExchange) throws IOException {
+		if (httpExchange.getRequestMethod().equals("GET")) {
+			if (httpExchange.getRequestURI().getPath().equals("/")) {//
+				Http.sendHtml(httpExchange, "/index.html");
+			} else {
+				Http.sendRaw(httpExchange, httpExchange.getRequestURI().getPath());
+			}
+		} else if (httpExchange.getRequestMethod().equals("POST")) {
+			String username = null;
+			String password = null;
+			{
+				String query = Http.getPOST(httpExchange.getRequestBody());
+				String[] array = query.split("&");
+				for (String str : array) {
+					String[] pair = str.split("=");
+					if (pair[0].equals("username")) {
+						username = pair[1];
+					} else if (pair[0].equals("password")) {
+						password = pair[1];
+					}
+				}
+			}
+			try {
+				if (DatabaseConnection.validCredentials(username, password)) {
+					String sessionID = DatabaseConnection.makeCookie(username, password);
+					Headers headers = httpExchange.getResponseHeaders();
+					headers.set("Set-Cookie", String.format("%s=%s; path=/app", "sessionID", sessionID));
+					headers.set("Location", "/app");
 
-                    httpExchange.sendResponseHeaders(303, -1);
-                } else {
-                    Http.send401(httpExchange);
-                }
-            } catch (Exception e) {
-                System.out.println("Error: " + e.getMessage());
-                e.printStackTrace();
-                Http.send500(httpExchange);
-            }
-        }
-    }
+					httpExchange.sendResponseHeaders(303, -1);
+				} else {
+					Http.send401(httpExchange);
+				}
+			} catch (Exception e) {
+				System.out.println("Error: " + e.getMessage());
+				e.printStackTrace();
+				Http.send500(httpExchange);
+			}
+		}
+	}
 }
 
