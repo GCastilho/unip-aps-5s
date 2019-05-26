ac86042a64bcdcc5ab9b29321f01bcd1181f6993
gabriel <gabrielgustavorodrigues@gmail.com>
2019-05-25 23:49:33 -0300

    created new funcions on mongoConnection
    --add function that add message on the right collection
    --add function that adds a new chat, this function MUST be called when a new chat is created
    --add function that gets ALL the messages of the chat in the database as Array list of Document(basically Map)
    --added test db files for mongo, this files are outdated and will be removed/altered later
    --TODO: make a function that only gets the x to y messages from the mongoDb

--- src/database/MongoConnection.java
+++ src/database/MongoConnection.java
@@ -1,18 +1,57 @@
 package database;
 
 import com.mongodb.*;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.MongoClient;
+import com.mongodb.client.*;
+
 import org.bson.Document;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import static com.mongodb.client.model.Filters.regex;
+
 public class MongoConnection {
-	private final static String dbName = "app";
-	private final static String tableName = "message";
+	private final static String dbName = "message";
 
 	private static MongoClient mongoClient = new MongoClient("localhost", 27017);
 	private static MongoDatabase database = mongoClient.getDatabase(dbName);
 
-	public static void addMessage(Document message){
-		database.getCollection(tableName).insertOne(message);
+	public static void addMessage(Document message,String sender, String receiver){
+
+		database.getCollection(alfabeticalOrder(sender,receiver)).insertOne(message);
 	}
+
+	public static List<Document> getChatMessageList (String sender, String receiver){
+		//the -1 on sort means that the get order is desc
+		return	database.getCollection(alfabeticalOrder(sender,receiver)).find().
+						sort(new BasicDBObject("_id",-1)).into(new ArrayList<>());
+
+	}
+
+	public static void addChat(String user1, String user2){
+
+		String chatId = alfabeticalOrder(user1,user2);
+		Document doc = new Document();
+		doc.put("chat",chatId);
+		database.getCollection("index").insertOne(doc);
+	}
+
+	public static List<Document> getUserChatList (String user){
+		return database.getCollection("index").
+				find(regex("chat", ".*" +Pattern.quote(user)+".*")).into(new ArrayList<>());
+
+	}
+
+	private static String alfabeticalOrder(String imput1, String imput2){
+		String[] vector = new String[2];
+		vector[0] = imput1;
+		vector[1] = imput2;
+		Arrays.sort(vector);
+		return ""+vector[0]+"-"+vector[1]+"";
+	}
+
 }
 
